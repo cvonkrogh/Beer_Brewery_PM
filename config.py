54536503543be1eb81379f1faf6ff1a34f635714
cvonkrogh <c.vonkrogh@outlook.com>
2026-02-24 17:04:06 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# config.py
+
+CORE_BEERS = [
+    "Hoop US Lager",
+    "Hoop Bleke Nelis",
+    "Hoop BulleBier",
+    "Hoop Kaper North East IPA",
+    "Hoop Anker"
+]
+
+RAW_DATA_PATH = "data/raw/sales_data.csv"
+PROCESSED_DATA_PATH = "data/processed/weekly_beer_data.csv"
+
+WEATHER_LAT = 52.3676  # Amsterdam (change if needed)
+WEATHER_LON = 4.9041
